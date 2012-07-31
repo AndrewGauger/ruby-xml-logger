933d5d4b67a60713d167c47389cfa2482fd51731
Andrew Gauger <andrew@alltech1.com>
2012-07-31 08:37:51 -0700

    Log rotate for Heroku - simple

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,15 +2,21 @@ require 'nokogiri'
 
 class Document < ActiveRecord::Base
 	attr_accessible :orderNumber, :xmlDoc
-
-
 	validates :xmlDoc, :presence => true
-
 	before_create :set_orderNumber
 
 	def set_orderNumber
 		@xml = Nokogiri::XML::Document.parse(self.xmlDoc.to_s)
 		self.orderNumber = @xml.xpath("//Order//SalesHeader//SalesId").text
+
+		#handles log rotation, Implemented once Heroku hit 4M
+		@oldest_document = Document.first(:order => 'id asc')
+		@oldest_document.destroy
 	end
+
+
+
+	#def link_to_doc
+	#	link_to "Document" , 
 
 end
